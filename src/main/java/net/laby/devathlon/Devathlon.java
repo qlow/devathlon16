154eaaf75fba780043f6e2887952917d4b0996ab
Jan <me@qlow.eu>
2016-07-16 11:57:38 +0200

    Added main-class

---
+++ src/main/java/net/laby/devathlon/Devathlon.java
@@ -0,0 +1,17 @@
+package net.laby.devathlon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Class created by qlow | Jan
+ */
+public class Devathlon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
