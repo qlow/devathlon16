3beab12b7fae305976bff87bb376c033ee20625b
Jan <me@qlow.eu>
2016-07-16 19:59:11 +0200

    Added JavaDocs to PortalWand & Portal

--- src/main/java/net/laby/devathlon/utils/Portal.java
+++ src/main/java/net/laby/devathlon/utils/Portal.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  * Class created by qlow | Jan
- * Represents a portal
+ * Represents a portal (a portal has 2 blocks)
  */
 public class Portal {
 
@@ -20,18 +20,34 @@ public class Portal {
     private Block secondBlock;
     private BlockFace blockFace;
 
+    /**
+     * @param portalType  portal's type
+     * @param firstBlock  portal's first old block
+     * @param secondBlock portal's second old block
+     */
     public Portal( PortalType portalType, Block firstBlock, Block secondBlock ) {
         this.portalType = portalType;
         this.firstBlock = firstBlock;
         this.secondBlock = secondBlock;
     }
 
+    /**
+     * @param blockFace   portal's teleport-blockface
+     * @param firstBlock  portal's first old block
+     * @param secondBlock portal's second old block
+     */
     public Portal( BlockFace blockFace, Block firstBlock, Block secondBlock ) {
         this( PortalType.WALL, firstBlock, secondBlock );
 
         this.blockFace = blockFace;
     }
 
+    /**
+     * State whether the player is in the portal (for teleporting)
+     *
+     * @param player player the state should get requested from
+     * @return true if the player is in the portal
+     */
     public boolean isInPortal( Player player ) {
         Location loc = player.getLocation();
 
@@ -49,35 +65,40 @@ public class Portal {
                 return (onXAxis || onZAxis) && onY;
         }
 
+        // Can't happen
         return false;
     }
 
+    /**
+     * Teleporting player through the portal
+     *
+     * @param player player should be teleported
+     */
     public void teleport( Player player ) {
         switch ( portalType ) {
             case FLOOR:
+                // Teleporting
                 player.teleport( firstBlock.getLocation().clone().add( 0, 3, 0 ) );
+
+                // Setting velocity
                 player.setVelocity( new Vector( 0, 1, 0 ) );
                 break;
             case WALL:
-                AnglesToLocationCalculator anglesToLocationCalculator =
-                        new AnglesToLocationCalculator( firstBlock.getRelative( getBlockFace(), 2 ).getLocation(), firstBlock.getLocation() );
+                // Teleporting
+                player.teleport( firstBlock.getRelative( getBlockFace(), 2 ).getLocation() );
 
-                Location teleportTo = firstBlock.getRelative( getBlockFace(), 2 ).getLocation();
-                teleportTo.setYaw( ( float ) anglesToLocationCalculator.getYaw() );
-                teleportTo.setPitch( ( float ) anglesToLocationCalculator.getPitch() );
-
-                player.teleport( teleportTo );
+                // Setting velocity
                 player.setVelocity( new Vector( getBlockFace().getModX() * 2, 0, getBlockFace().getModZ() * 2 ) );
                 break;
         }
     }
 
+    /**
+     * List of all portal-blocks
+     * @return new list with {@link Portal#firstBlock} and {@link Portal#secondBlock}
+     */
     public List<Block> getPortalBlocks() {
         return Arrays.asList( firstBlock, secondBlock );
-    }
-
-    public PortalType getPortalType() {
-        return portalType;
     }
 
     public Block getFirstBlock() {
@@ -88,14 +109,20 @@ public class Portal {
         return secondBlock;
     }
 
-    public BlockFace getBlockFace() {
-        return blockFace;
+    /**
+     * Type of portal
+     * @return portal's type
+     */
+    public PortalType getPortalType() {
+        return portalType;
     }
 
-    public static enum PortalType {
-        FLOOR, WALL;
-    }
-
+    /**
+     * Calculates the blockface/direction from
+     * @param from
+     * @param to
+     * @return
+     */
     public static BlockFace getBlockFaceFromTo( Location from, Location to ) {
         int x = from.getBlockX() < to.getBlockX() ? -1 : 1;
         int z = from.getBlockZ() < to.getBlockZ() ? -1 : 1;
@@ -121,33 +148,16 @@ public class Portal {
             return blockFaces.getOppositeFace();
         }
 
+        // Shouldn't happen...
         return BlockFace.NORTH;
     }
 
-    private static class AnglesToLocationCalculator {
+    public BlockFace getBlockFace() {
+        return blockFace;
+    }
 
-        private double yaw;
-        private double pitch;
-
-        public AnglesToLocationCalculator( Location from, Location to ) {
-            double dx = to.getX() - from.getX();
-            double dy = to.getY() - from.getY();
-            double dz = to.getZ() - from.getZ();
-            double r = Math.sqrt( dx * dx + dy * dy + dz * dz );
-            yaw = -Math.atan2( dx, dz ) / Math.PI * 180;
-            if ( yaw < 0 )
-                yaw = 360 - yaw;
-
-            pitch = -Math.asin( dy / r ) / Math.PI * 180;
-        }
-
-        public double getYaw() {
-            return yaw;
-        }
-
-        public double getPitch() {
-            return pitch;
-        }
+    public static enum PortalType {
+        FLOOR, WALL;
     }
 
 }
--- src/main/java/net/laby/devathlon/wand/PortalWand.java
+++ src/main/java/net/laby/devathlon/wand/PortalWand.java
@@ -26,11 +26,10 @@ public class PortalWand extends Wand {
 
     private Portal firstPortal;
     private Portal secondPortal;
-
-    private static int counter = 1337 * 9;
+    private int tickCounter = 0;
 
     public PortalWand( Player player ) {
-        super( player, Material.STICK, 0, "§9Portal§7-§cStab", "", "§7-> Mit Rechts & Linksklick kannst du", "  §7Portale setzen, durch die", "  §7man laufen kann.", "" );
+        super( player, Material.STICK, 0, "§9Portal§7-§cStab", "", "§7-> Mit Rechts & Linksklick kannst du", "   §7Portale setzen, durch die man", "   §7laufen kann.", "" );
 
         this.player = player;
     }
@@ -47,11 +46,13 @@ public class PortalWand extends Wand {
 
     @Override
     public void onRightClick() {
+        // Making second-portal
         makePortal( false );
     }
 
     @Override
     public void onLeftClick() {
+        // Making first-portal
         makePortal( true );
     }
 
@@ -60,22 +61,28 @@ public class PortalWand extends Wand {
         if ( firstPortal == null || secondPortal == null )
             return;
 
+        // Checking every 5 ticks whether the player is in the portal
         if ( tickCounter++ % 5 != 0 ) {
             return;
         }
 
         if ( firstPortal.isInPortal( player ) ) {
+            // Teleporting player to second portal if he is in the first portal
             secondPortal.teleport( player );
         } else if ( secondPortal.isInPortal( player ) ) {
+            // Teleporting player to first portal if he is in the second portal
             firstPortal.teleport( player );
         }
 
         tickCounter = 0;
     }
 
-    private int tickCounter = 0;
-
+    /**
+     * Makes the portal
+     * @param left true if it is the first portal
+     */
     private void makePortal( boolean left ) {
+        // Getting line of sight (up to 50 blocks)
         List<Block> blocks = player.getLineOfSight( ( HashSet<Byte> ) null, 50 );
         Block target = null;
 
@@ -84,56 +91,74 @@ public class PortalWand extends Wand {
                 continue;
             }
 
+            // Setting target-block
             target = block;
             break;
         }
 
+        // Returning if there is no target
         if ( target == null )
             return;
 
+        // Last block before the target
         Location lastBlock = player.getLocation();
 
+        // Iterating through the line of sight
         for ( Block block : blocks ) {
             Location location = block.getLocation();
 
             if ( block.getType() == Material.AIR || block.getType() == Material.WATER || block.getType() == Material.STATIONARY_WATER ) {
+                // Playing effect if it is air
                 player.playEffect( location, Effect.MOBSPAWNER_FLAMES, 0 );
                 lastBlock = block.getLocation();
                 continue;
             }
 
+            // Breaking if the target is reached
             if ( block == target )
                 break;
         }
 
+        // List of blocks that will be replaced
         List<Location> portalBlocks = new ArrayList<>();
+
+        // Instance of portal
         Portal portal = null;
 
         if ( target.getLocation().clone().add( 0, 1, 0 ).getBlock().getType() != Material.AIR ) {
+            // Adding blocks to portal-blocks-list
             portalBlocks.add( target.getLocation() );
             portalBlocks.add( target.getLocation().add( 0, 1, 0 ) );
 
+            // Initializing portal
             portal = new Portal( Portal.getBlockFaceFromTo( target.getLocation(), lastBlock ), portalBlocks.get( 0 ).getBlock(), portalBlocks.get( 1 ).getBlock() );
         } else {
+            // Getting valid block near the target-block
             Location secondBlock = getSecondBlockLocationOnFloor( target.getLocation() );
 
+            // Returning if there is no valid block near the target
             if ( secondBlock == null )
                 return;
 
+            // Adding blocks to portal-blocks-list
             portalBlocks.add( target.getLocation() );
             portalBlocks.add( secondBlock );
 
+            // Initializing portal
             portal = new Portal( Portal.PortalType.FLOOR, portalBlocks.get( 0 ).getBlock(), portalBlocks.get( 1 ).getBlock() );
         }
 
+        // Sending default-blocks of the selected portal
         sendDefaultBlocks( left );
 
+        // Setting private portal-variable
         if ( left ) {
             firstPortal = portal;
         } else {
             secondPortal = portal;
         }
 
+        // Iterating through the portal-blocks
         for ( Location portalBlock : portalBlocks ) {
             byte data = 6;
 
@@ -141,11 +166,14 @@ public class PortalWand extends Wand {
                 data = 11;
             }
 
+            // Creating block-change-packet
             PacketContainer packetContainer = ProtocolLibrary.getProtocolManager().createPacket( PacketType.Play.Server.BLOCK_CHANGE );
 
+            // Writing data into the packet
             packetContainer.getBlockPositionModifier().write( 0, new BlockPosition( portalBlock.getBlockX(), portalBlock.getBlockY(), portalBlock.getBlockZ() ) );
             packetContainer.getBlockData().write( 0, WrappedBlockData.createData( Material.STAINED_CLAY, data ) );
 
+            // Sending packet to player
             try {
                 ProtocolLibrary.getProtocolManager().sendServerPacket( player, packetContainer );
             } catch ( InvocationTargetException e ) {
@@ -155,18 +183,31 @@ public class PortalWand extends Wand {
 
     }
 
+    /**
+     * Sends the blockchange-packets so the player can see the old blocks again
+     *
+     * @param left
+     */
     private void sendDefaultBlocks( boolean left ) {
+        // List of blocks that should be reset
         List<Block> reset = new ArrayList<>();
+
+        // Adding to reset-list
         if ( left && firstPortal != null ) reset.addAll( firstPortal.getPortalBlocks() );
         if ( !left && secondPortal != null ) reset.addAll( secondPortal.getPortalBlocks() );
 
+        // Iterating through reset-list
         for ( Block resetBlocks : reset ) {
             Location loc = resetBlocks.getLocation();
+
+            // Creating PacketContainer for Block-Change
             PacketContainer packetContainer = ProtocolLibrary.getProtocolManager().createPacket( PacketType.Play.Server.BLOCK_CHANGE );
 
+            // Writing data to the packet
             packetContainer.getBlockPositionModifier().write( 0, new BlockPosition( loc.getBlockX(), loc.getBlockY(), loc.getBlockZ() ) );
             packetContainer.getBlockData().write( 0, WrappedBlockData.createData( resetBlocks.getType(), resetBlocks.getData() ) );
 
+            // Sending packet
             try {
                 ProtocolLibrary.getProtocolManager().sendServerPacket( player, packetContainer );
             } catch ( InvocationTargetException e ) {
@@ -174,11 +215,19 @@ public class PortalWand extends Wand {
             }
         }
 
+        // Clearing portal
         if ( left ) firstPortal = null;
         if ( !left ) secondPortal = null;
     }
 
+    /**
+     * A valid portal-location near the given location
+     *
+     * @param firstBlock given location
+     * @return valid location near the given location
+     */
     public Location getSecondBlockLocationOnFloor( Location firstBlock ) {
+        // Array of all blocks near the given block
         Location[] nearbyBlocks = new Location[]{
                 firstBlock.clone().add( 1, 0, 0 ),
                 firstBlock.clone().add( 0, 0, 1 ),
@@ -186,13 +235,17 @@ public class PortalWand extends Wand {
                 firstBlock.clone().subtract( 0, 0, 1 )
         };
 
+        // Iterating through all blocks near the given block
         for ( Location nearbyBlock : nearbyBlocks ) {
+            // Checking if there is something above the current block
             if ( nearbyBlock.clone().add( 0, 1, 0 ).getBlock().getType() != Material.AIR )
                 continue;
 
+            // Returning that block if there is space over it
             return nearbyBlock;
         }
 
+        // Returning null if there is no valid block
         return null;
     }
 
