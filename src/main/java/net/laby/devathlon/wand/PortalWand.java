73926cc38d9ec4a1991f46fc2629791e7bfbc963
Jan <me@qlow.eu>
2016-07-16 16:43:35 +0200

    Removed slime-stuff.

--- src/main/java/net/laby/devathlon/wand/PortalWand.java
+++ src/main/java/net/laby/devathlon/wand/PortalWand.java
@@ -1,20 +1,15 @@
 package net.laby.devathlon.wand;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * Class created by qlow | Jan
@@ -58,55 +53,7 @@ public class PortalWand extends Wand {
             Location location = block.getLocation();
 
             if ( block.getType() == Material.AIR || block.getType() == Material.WATER || block.getType() == Material.STATIONARY_WATER ) {
-                // Spawning a slime
-                PacketContainer packetContainer = ProtocolLibrary.getProtocolManager().createPacket( PacketType.Play.Server.SPAWN_ENTITY_LIVING );
-                packetContainer.getIntegers().write( 0, ++counter );                    // Entity-ID
-                packetContainer.getSpecificModifier( UUID.class ).write( 0, UUID.randomUUID() );  // Entity-UUID
-                packetContainer.getIntegers().write( 1, 55 );                           // Entity's type
-                packetContainer.getDoubles().write( 0, location.getX() )                // X-position
-                        .write( 1, location.getY() )                                    // Y-position
-                        .write( 2, location.getZ() );                                   // Z-position
-
-                packetContainer.getIntegers().write( 2, 0 );                      // Angle
-                packetContainer.getIntegers().write( 3, 0 );                      // Angle
-                packetContainer.getIntegers().write( 4, 0 );                      // Angle
-
-                packetContainer.getBytes().write( 0, (byte) 0 );                      // Vector-X
-                packetContainer.getBytes().write( 1, (byte) 0 );                      // Vector-Y
-                packetContainer.getBytes().write( 2, (byte) 0 );                      // Vector-Z
-
-                WrappedDataWatcher dataWatcher = new WrappedDataWatcher();
-
-                // A lot metadata stuff
-                dataWatcher.setObject( 0, ( byte ) (0 | 1 << 5) );  // Invisibility
-                dataWatcher.setObject( 7, 0 );                    // Potion effect color
-                dataWatcher.setObject( 8, ( byte ) 0 );             // Is potion effect active?
-                dataWatcher.setObject( 9, ( byte ) 0 );             // Number of Arrows
-                dataWatcher.setObject( 6, ( float ) 1.0f );         // Health of entity
-                dataWatcher.setObject( 16, ( byte ) 1 );            // Slime size
-
-                packetContainer.getDataWatcherModifier().write( 0, dataWatcher );       // Metadata
-
-                try {
-                    ProtocolLibrary.getProtocolManager().sendServerPacket( player, packetContainer );
-                } catch ( InvocationTargetException e ) {
-                    e.printStackTrace();
-                }
-
-                // Sending potion-packet
-                PacketContainer potionPacketContainer = ProtocolLibrary.getProtocolManager().createPacket( PacketType.Play.Server.ENTITY_EFFECT );
-
-                potionPacketContainer.getIntegers().write( 0, counter );
-                potionPacketContainer.getBytes().write( 0, ( byte ) 24 ).write( 1, ( byte ) 1 );
-                potionPacketContainer.getIntegers().write( 1, Integer.MAX_VALUE );
-                potionPacketContainer.getBooleans().write( 0, true );
-
-                try {
-                    ProtocolLibrary.getProtocolManager().sendServerPacket( player, potionPacketContainer );
-                } catch ( InvocationTargetException e ) {
-                    e.printStackTrace();
-                }
-
+                player.playEffect( location, Effect.MOBSPAWNER_FLAMES, 0 );
                 continue;
             }
         }
