233cf5f852a10eaf3175b2f94b1d291f32ad287d
Jan <me@qlow.eu>
2016-07-30 14:15:50 +0200

    Testing

--- round-3/src/main/java/net/laby/devathlon/TestingClass.java
+++ round-3/src/main/java/net/laby/devathlon/TestingClass.java
@@ -2,7 +2,6 @@ package net.laby.devathlon;
 
 import net.minecraft.server.v1_10_R1.EntityPlayer;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
@@ -11,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -53,34 +53,28 @@ public class TestingClass implements Listener {
                     Entity vehicle = player.getVehicle();
                     EntityPlayer entityPlayer = (( CraftPlayer ) player).getHandle();
 
-                    Location newLocation = null;
+                    float addX = 0;
+                    float addZ = 0;
 
                     if ( entityPlayer.bf != 0F ) {
-                        if ( newLocation == null ) {
-                            newLocation = vehicle.getLocation().clone();
-                        }
+                        float distance = entityPlayer.bf * -1;
 
-                        int distance = (entityPlayer.bf > 0F ? -10 : 10);
-
-                        float newX = ( float ) (vehicle.getLocation().getX() + (distance * Math.cos( Math.toRadians( vehicle.getLocation().getYaw() + 90 * 0 ) )));
-                        float newZ = ( float ) (vehicle.getLocation().getZ() + (distance * Math.sin( Math.toRadians( vehicle.getLocation().getYaw() + 90 * 0 ) )));
-
-                        Bukkit.broadcastMessage( "new x: " + newX + " new z: " + newZ );
-
-                        newLocation.setX( newX );
-                        newLocation.setZ( newZ );
+                        addX += ( float ) (distance * Math.cos( Math.toRadians( vehicle.getLocation().getYaw() + 90 * 0 ) ));
+                        addZ += ( float ) (distance * Math.sin( Math.toRadians( vehicle.getLocation().getYaw() + 90 * 0 ) ));
                     }
 
-                    if ( newLocation != null )
-                        vehicle.teleport( newLocation );
-
+                    if ( addX != 0 || addZ != 0 ) {
+                        Vector vector = vehicle.getLocation().getDirection();
+                        vehicle.setVelocity( new Vector( vector.getX() * addX, 0D, vector.getZ() * addZ ) );
+                        Bukkit.broadcastMessage( "Setting velocity" );
+                    }
 
                     Bukkit.broadcastMessage( player.getName() + ": §cbf: " + entityPlayer.bf + " §abg: " + entityPlayer.bg );
                 }
 
                 vehiclePlayers.removeAll( removeAll );
             }
-        }.runTaskTimer( plugin, 20L, 5L );
+        }.runTaskTimer( plugin, 20L, 1L );
     }
 
     @EventHandler
