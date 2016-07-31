455ec704041329b5f43f885b5e04a760fc7689e1
Jan <me@qlow.eu>
2016-07-31 04:52:20 +0200

    Fixed a very stupid bug

--- round-3/src/main/java/net/laby/game/listeners/InteractListener.java
+++ round-3/src/main/java/net/laby/game/listeners/InteractListener.java
@@ -80,12 +80,13 @@ public class InteractListener implements Listener {
                         hitPlayer.setLife( hitPlayer.getLife() - gamePlayer.getAttackDamage() );
 
                         if ( hitPlayer.getLife() <= 0 ) {
-                            hitPlayer.setIngame( false );
                             player.sendMessage( "§7Du hast §6" + hitPlayer.getPlayer().getName() + " §7getötet!" );
                             hitPlayer.getPlayer().sendMessage( "§7Du wurdest von §6" + player.getName() + " §7getötet!" );
 
                             hitPlayer.getPlayer().getVehicle().setPassenger( null );
                             hitPlayer.leaveGame();
+
+                            hitPlayer.setIngame( false );
 
                             gamePlayer.setKillStreak( gamePlayer.getKillStreak() + 1 );
                             Game.getGame().getGameScoreboardManager().updateScoreboard( player );
